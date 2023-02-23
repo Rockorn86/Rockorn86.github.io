6319a57a6b71e52f5b6d1ada2b38a79751067510
Rockorn86 <53450559+Rockorn86@users.noreply.github.com>
2023-02-23 10:36:35 -0300

    trabajo final
    Curriculum vitae

---
+++ js/function.js
@@ -0,0 +1,37 @@
+window.addEventListener("DOMContentLoaded",gmail)
+function gmail(){
+    var imgs = [" "];
+    var contacto = document.getElementById("contacto");
+ 
+      contacto.addEventListener("click",function(){
+        alert("RodrigoLionelPerk@gmail.com");
+      });
+
+      var contactouno = document.getElementById("contactouno");
+ 
+      contactouno.addEventListener("click",function(){
+        alert("http://www.linkedin.com/in/rodrigo-perk-30143047");
+      });
+
+      var contactodos = document.getElementById("contactodos");
+ 
+      contactodos.addEventListener("click",function(){
+        alert("https://twitter.com/RodrigoPerk");
+      });
+
+      var contactotres = document.getElementById("contactotres");
+ 
+      contactotres.addEventListener("click",function(){
+        alert("https://www.facebook.com/Rodrigo.Perk");
+      });
+
+      var contactocuatro = document.getElementById("contactocuatro");
+ 
+      contactocuatro.addEventListener("click",function(){
+        alert("https://t.me/Perk86");
+      });
+
+    }
+  
+
+

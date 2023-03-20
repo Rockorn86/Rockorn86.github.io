0553fdf5ba3057997b3ca5d1bc0dcb4be78ac941
Rockorn86 <53450559+Rockorn86@users.noreply.github.com>
2023-03-20 00:00:45 -0300

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -1,37 +1,123 @@
-window.addEventListener("DOMContentLoaded",gmail)
-function gmail(){
+//RV:Menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+//RV:oculto el menu una vez que selecciono una opción
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
 
-    var contacto = document.getElementById("gmail");
- 
-      contacto.addEventListener("click",function(){
-        alert("RodrigoLionelPerk@gmail.com");
-      });
+//Creo las barritas de una barra particular identificada por su id
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
 
-      var contactouno = document.getElementById("contactouno");
- 
-      contactouno.addEventListener("click",function(){
-        alert("http://www.linkedin.com/in/rodrigo-perk-30143047");
-      });
+//RV: selecciono todas las barras generales par aluego manipularlas
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let wordpress = document.getElementById("wordpress");
+crearBarra(wordpress);
+let photoshop = document.getElementById("photoshop");
+crearBarra(photoshop);
+let php = document.getElementById("php");
+crearBarra(php);
+let ilustrator = document.getElementById("ilustrator");
+crearBarra(ilustrator);
 
-      var contactodos = document.getElementById("contactodos");
- 
-      contactodos.addEventListener("click",function(){
-        alert("https://twitter.com/RodrigoPerk");
-      });
+//rv:función que aplica las animaciones de la habilidades
+let contadores = [-1,-1,-1,-1,-1,-1];
+let entro = false;
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills>=300 && entro==false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintarBarra(html, 16, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintarBarra(javascript, 11, 1, intervalJavascript);
+        },100);
+        const intervalWordpress = setInterval(function(){
+            pintarBarra(wordpress, 11, 2, intervalWordpress);
+        },100);
+        const intervalPhotoshop = setInterval(function(){
+            pintarBarra(photoshop, 15, 3, intervalPhotoshop);
+        },100);
+        const intervalPhp = setInterval(function(){
+            pintarBarra(php, 16, 4, intervalPhp);
+        },100);
+        const intervalIlustrator = setInterval(function(){
+            pintarBarra(ilustrator, 11, 5, intervalIlustrator);
+        },100);
+    }
+}
 
-      var contactotres = document.getElementById("contactotres");
- 
-      contactotres.addEventListener("click",function(){
-        alert("https://www.facebook.com/Rodrigo.Perk");
-      });
+//rv:lleno una barra particular con la cantidad indicada
+function pintarBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x = contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#110294";
+    }else{
+        clearInterval(interval)
+    }
+}
 
-      var contactocuatro = document.getElementById("contactocuatro");
- 
-      contactocuatro.addEventListener("click",function(){
-        alert("https://t.me/Perk86");
-      });
+//rv:detecto el scrolling del mouse para aplicar la animación de la barra
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
+//rv:Valido formulario de contacto
+function valida_envia()
+{
+
+
+    if(document.formDatosPersonales.email.value.length==0)
+    {
+        alert("Tiene que ingresar un mail.")
+        document.formDatosPersonales.email.focus()
+        return 0;
 
     }
-  
 
+    if(document.formDatosPersonales.asunto.value.length==0)
+    {
+        alert("Tiene que ingresar un asunto.")
+        document.formDatosPersonales.asunto.focus()
+        return 0;
 
+    }
+
+    if(document.formDatosPersonales.mensaje.value.length==0)
+    {
+        alert("Tiene que ingresar un mensaje.")
+        document.formDatosPersonales.mensaje.focus()
+        return 0;
+
+    }
+
+    alert("El mensaje fue enviado correctamente!!");
+    document.formDatosPersonales.submit();
+}
